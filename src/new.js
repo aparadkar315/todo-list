74be51ecfec6f1e89755bfe7f0a17d3effb162c5
Abhishek Paradkar <aparadkar315@gmail.com>
2026-01-20 15:35:23 +0530

    Create a class to create todo items

---
+++ src/new.js
@@ -0,0 +1,57 @@
+export class Item {
+    constructor(title, description, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+
+    get title() {
+        return this._title;
+    }
+
+    get description() {
+        return this._description;
+    }
+
+    get dueDate() {
+        return this._dueDate;
+    }
+
+    get priority() {
+        return this._priority;
+    }
+
+    set title(value) {
+        if (value.length < 1) {
+        alert("Can't be blank");
+        return;
+        }
+        this._title = value;
+    }
+
+    set description(value) {
+        if (value.length < 1) {
+        alert("Can't be blank");
+        return;
+        }
+        this._description = value;
+    }
+
+    set dueDate(value) {
+        if (value.length < 1) {
+        alert("Can't be blank");
+        return;
+        }
+        this._dueDate = value;
+    }
+
+    set priority(value) {
+        if (value.length < 1) {
+        alert("Can't be blank");
+        return;
+        }
+        this._priority = value;
+    }
+
+}

efbc111f617ffe14ba62ae4697ba8643a7809385
Abhishek Paradkar <aparadkar315@gmail.com>
2026-02-09 12:17:51 +0530

    Create classe Listoflist, CreateList and Task

--- src/list.js
+++ src/list.js
@@ -1,14 +1,62 @@
-export class CreateList {
-    constructor() {
-            this.arr = [];
+export {Listoflist, CreateList, Task}
+
+
+ class Listoflist {
+
+}
+
+class CreateList {
+    
+}
+
+class Task {
+
+    get title() {
+        return this._title;
+    }
+
+    get description() {
+        return this._description;
+    }
+
+    get dueDate() {
+        return this._dueDate;
+    }
+
+    get priority() {
+        return this._priority;
+    }
+
+    set title(value) {
+        if (value.length < 1) {
+        alert("Can't be blank");
+        return;
         }
-
-
-    addTolist(item) {
-        this.arr.push(item);
+        this._title = value;
     }
 
-    getList() {
-        return this.arr;
+    set description(value) {
+        if (value.length < 1) {
+        alert("Can't be blank");
+        return;
+        }
+        this._description = value;
     }
+
+    set dueDate(value) {
+        if (value.length < 1) {
+        alert("Can't be blank");
+        return;
+        }
+        this._dueDate = value;
+    }
+
+    set priority(value) {
+        if (value.length < 1) {
+        alert("Can't be blank");
+        return;
+        }
+        this._priority = value;
+    }
+
 }
